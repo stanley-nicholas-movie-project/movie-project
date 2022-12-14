33172afe5485b889113a1af957aad5866411749c
Matthew Stanley <matthew.stanley35t@gmail.com>
2022-12-14 15:49:18 -0700

    final version of movie project

--- js/moviesJs.js
+++ js/moviesJs.js
@@ -8,6 +8,7 @@ let selectedGenre;
 let selectedPlot;
 let selectedRating;
 let selectedDirector;
+let selectedPoster;
 let movies = [];
 let randomOrder = [];
 let uniqueId;
@@ -15,6 +16,8 @@ let sortedMovies;
 let searchRating = "all";
 let searchGenre = "all";
 
+
+// poster = "../assets/N:A.jpeg"
 
 //only returns movies with IDs
 function setMovies(){
@@ -41,6 +44,7 @@ function displayMovies(movies){
     $(`#movieCards`).empty();
     movies.forEach((m) => {
         let stars = ratingStars(m.rating);
+        let imdbStars = ratingStars(m.iRating);
         $(`#movieCards`).append(`<div class="col my-3 ">
                 <div class="card click-me h-100" id="${m.id}">
                     <img src="${m.poster}" class="card-img-top img-fluid" alt="">
@@ -51,7 +55,8 @@ function displayMovies(movies){
                     <ul class="list-group list-group-flush">
                         <li class="list-group-item">Genre: ${m.genre}</li>
                         <li class="list-group-item">Director: ${m.director}</li>
-                        <li class="list-group-item">Rating: ${m.rating} <img id="ratingStarsImg" src=${stars} class="img-fluid" alt=""></li>
+                        <li class="list-group-item">My Rating: ${m.rating} <img id="ratingStarsImg" src=${stars} class="img-fluid" alt=""></li>
+                        <li class="list-group-item">IMDb Rating: ${m.iRating} <img id="ratingStarsImg" src=${imdbStars} class="img-fluid" alt=""></li>
                     </ul>
                     <div class="card-footer d-flex justify-content-between">
                         <button type="button" class="btn btn-primary editMovieBtn" data-bs-toggle="modal" data-bs-target="#editMovieModal">Edit Movie</button>
@@ -132,6 +137,32 @@ $(`#sortRatingBtn`).click(function(){
 
 $("#addMovieSubmit").click(function(){
     sendNewMovie();
+})
+
+$(`#addNewBtn`).click(function(){
+    $(`#autofillHelp`).remove();
+    $(`.reset`).val("");
+    $(`#previewImg`).attr("src", "");
+    $("#ratingBadge").removeClass (function (index, className) {
+        return (className.match (/(^|\s)rating\S+/g) || []).join(' ');
+    }).addClass("rating25").val(2.5);
+    $(`#addMovieSubmit`).addClass("invisible");
+})
+
+$(`#resetSort`).click(function(){
+    $(`#movieSearchByTitle`).val("");
+    $('#movieSearchByRating').val("all")
+    $('#movieSearchByGenre').val("all")
+    searchGenre = "all";
+    searchRating = "all";
+    resetSortByTitle();
+    resetSortByRating();
+    nameDefault = false;
+    nameZ = true;
+    sortByTitle();
+    ratingDefault = false;
+    ratingUp = true;
+    sortByRating();
 })
 
 
@@ -273,6 +304,7 @@ function updateEventHandlers(){
                 selectedPlot = m.plot;
                 selectedRating = m.rating;
                 selectedDirector = m.director;
+                selectedPoster = m.poster;
             }
         });
         $(`#toDeleteTitle`).append(`<p>Do you want to delete ${selectedTitle}?</p>`)
@@ -307,7 +339,20 @@ function editModalPreload(){
     $("#editMovieRating").val(selectedRating)
     $("#editMovieDirector").val(selectedDirector)
     $("#editMovieDescription").val(selectedPlot)
-    $("#editRatingOutput").val(selectedRating)
+    $("#editRatingOutput").val(selectedRating).removeClass (function (index, className) {
+        return (className.match (/(^|\s)rating\S+/g) || []).join(' ');
+    }).addClass(removeDecimals(selectedRating));
+}
+
+
+//function to remove decimals from rating
+function removeDecimals(rating){
+    let removedDecimals = rating.replace(/\./g, '');
+    let ratingClass = "rating" + removedDecimals;
+    if (rating === "0.5"){
+        ratingClass = "ratingHalf"
+    }
+    return ratingClass;
 }
 
 
@@ -320,32 +365,76 @@ function sendEditedMovie(){
         director: $("#editMovieDirector").val(),
         plot: $("#editMovieDescription").val(),
         id: selectedId,
+        poster: selectedPoster,
     }
-    changeData(editedMovie.id, editedMovie.title, editedMovie.director, editedMovie.rating, editedMovie.genre, editedMovie.plot)
+    changeData(editedMovie.id, editedMovie.title, editedMovie.director, editedMovie.rating, editedMovie.genre, editedMovie.plot, editedMovie.poster)
 }
+//// autofill
 
+$(`#autofill`).click(function(){
+    $(`#autofillHelp`).remove();
+    autoFill($(`#newMovieTitle`).val(),$(`#newMovieYear`).val());
+});
 
-//function to update a movie object on the db
-function changeData( id, title, director, rating, genre, plot){
+function autoFill(title, year){
+    $(`#innerAddMovieModal`).addClass(`blur`);
+    $(`#fetchingPoster`).removeClass("invisible");
+
     let poster;
+    let director;
+    let plot;
+    let iRating;
+
+    let url;
+    if (year ===""){
+        url = `http://www.omdbapi.com/?apikey=91940b5a&t=${title}`;
+    }
+    else{
+        url = `http://www.omdbapi.com/?apikey=91940b5a&t=${title}&y=${year}`
+    }
+
+
     let requestOptions = {
         method: 'GET',
         redirect: 'follow'
     };
-    
-    fetch("http://www.omdbapi.com/?apikey=91940b5a&t="+ title, requestOptions)
+    fetch(url, requestOptions)
         .then(response => response.json())
-        .then(result => {
-            if (result.Error === "Movie not found!") {
-                
-                poster = "../assets/N:A.jpeg"
+        .then((response) =>{
+            if (response.Error === "Movie not found!"){
+                return Promise.reject(response);
             }
             else{
-                poster = result.Poster
+                return response
             }
-            
+        })
+        .then(result => {
+            director = result.Director;
+            poster = result.Poster;
+            plot = result.Plot;
+            iRating = parseInt(result.imdbRating) / 2;
         }).then(()=>{
-        let raw =
+            $(`#newMovieDirector`).val(director);
+            $(`#newMovieDescription`).val(plot);
+            $(`#imdbRating`).val(iRating);
+            $(`#previewImg`).attr("src", poster).before(`<div id="autofillHelp" class="form-text">Not the movie you were thinking? Try adding a year and search again.</div>`);
+            $(`#posterPreview`).removeClass("invisible");
+            $(`#innerAddMovieModal`).removeClass(`blur`);
+            $(`#fetchingPoster`).addClass("invisible");
+            $(`#addMovieSubmit`).removeClass("invisible");
+        }).catch((err) =>{
+            console.log(err)
+            alert("Your Movie could not be found!");
+            poster = "../assets/N:A.jpeg"
+            $(`#previewImg`).attr("src", poster);
+            $(`#innerAddMovieModal`).removeClass(`blur`);
+            $(`#fetchingPoster`).addClass("invisible");
+    })
+}
+
+//function to update a movie object on the db
+function changeData( id, title, director, rating, genre, plot, poster){
+            let raw =
             {
                 id: id,
                 poster: poster,
@@ -367,32 +456,14 @@ function changeData( id, title, director, rating, genre, plot){
     
         fetch(`https://pine-inexpensive-honeysuckle.glitch.me/movies/${selectedId}`, requestOptions)
             .then( response => setMovies());
-    })
+
 }
 
 
 //function to create a new movie object
-function addNewMovie(url, title, director, rating, genre, plot){
-    let poster;
-    let requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-    
-    fetch("http://www.omdbapi.com/?apikey=91940b5a&t="+ title, requestOptions)
-        .then(response => response.json())
-        .then(result => {
-            if (result.Error === "Movie not found!") {
-        
-               poster = "../assets/N:A.jpeg"
-            }
-            else{
-                poster = result.Poster
-            }
-        
-        })
-        .then(() => {
-            let raw =
+function addNewMovie(url, title, director, rating, genre, plot, poster, iRating){
+
+                let raw =
                 {
                     id: uniqueId,
                     poster: poster,
@@ -401,6 +472,7 @@ function addNewMovie(url, title, director, rating, genre, plot){
                     rating: rating,
                     genre: genre,
                     plot: plot,
+                    iRating: iRating
                 }
             ;
     
@@ -411,20 +483,17 @@ function addNewMovie(url, title, director, rating, genre, plot){
                 },
                 body: JSON.stringify(raw),
             };
-            console.log(raw);
-    
-    
+
             fetch(url, requestOptions)
-                .then( response => setMovies());
-        }).then(()=>{
-        $("#newMovieTitle").val("")
-           $("#newMovieGenre").val("")
-            $("#movieRating").val("")
-           $("#newMovieDirector").val("")
-          $("#newMovieDescription").val("")
-    })
-    
-    
+                .then( response => setMovies())
+                .then(()=>{
+                            $("#newMovieTitle").val("")
+                               $("#newMovieGenre").val("")
+                                $("#movieRating").val("")
+                               $("#newMovieDirector").val("")
+                              $("#newMovieDescription").val("")
+    });
+
 }
 
 
@@ -436,8 +505,10 @@ function sendNewMovie(){
         rating: $("#movieRating").val(),
         director: $("#newMovieDirector").val(),
         plot: $("#newMovieDescription").val(),
+        poster: $("#previewImg").attr("src"),
+        iRating: $("#imdbRating").val(),
     }
-    addNewMovie(dbURL, newMovie.title, newMovie.director, newMovie.rating, newMovie.genre, newMovie.plot)
+    addNewMovie(dbURL, newMovie.title, newMovie.director, newMovie.rating, newMovie.genre, newMovie.plot, newMovie.poster, newMovie.iRating)
 }
 
 
@@ -470,6 +541,62 @@ document.addEventListener("input",
     }, true);
 
 
+//regex taken from here https://stackoverflow.com/questions/2644299/jquery-removeclass-wildcard
+$(`#movieRating`).change(function(){
+    $("#ratingBadge").removeClass (function (index, className) {
+        return (className.match (/(^|\s)rating\S+/g) || []).join(' ');
+    });
+    if($(`#movieRating`).val() === "0.5"){
+        $(`#ratingBadge`).addClass("ratingHalf");
+    } else if($(`#movieRating`).val() === "1") {
+        $(`#ratingBadge`).addClass("rating1");
+    } else if($(`#movieRating`).val() === "1.5") {
+        $(`#ratingBadge`).addClass("rating15");
+    } else if($(`#movieRating`).val() === "2") {
+        $(`#ratingBadge`).addClass("rating2");
+    } else if($(`#movieRating`).val() === "2.5") {
+        $(`#ratingBadge`).addClass("rating25");
+    } else if($(`#movieRating`).val() === "3") {
+        $(`#ratingBadge`).addClass("rating3");
+    } else if($(`#movieRating`).val() === "3.5") {
+        $(`#ratingBadge`).addClass("rating35");
+    } else if($(`#movieRating`).val() === "4") {
+        $(`#ratingBadge`).addClass("rating4");
+    } else if($(`#movieRating`).val() === "4.5") {
+        $(`#ratingBadge`).addClass("rating45");
+    } else if($(`#movieRating`).val() === "5") {
+        $(`#ratingBadge`).addClass("rating5");
+    }
+})
+
+$(`#editMovieRating`).change(function(){
+    $("#editRatingOutput").removeClass (function (index, className) {
+        return (className.match (/(^|\s)rating\S+/g) || []).join(' ');
+    });
+    if($(`#editMovieRating`).val() === "0.5"){
+        $(`#editRatingOutput`).addClass("ratingHalf");
+    } else if($(`#editMovieRating`).val() === "1") {
+        $(`#editRatingOutput`).addClass("rating1");
+    } else if($(`#editMovieRating`).val() === "1.5") {
+        $(`#editRatingOutput`).addClass("rating15");
+    } else if($(`#editMovieRating`).val() === "2") {
+        $(`#editRatingOutput`).addClass("rating2");
+    } else if($(`#editMovieRating`).val() === "2.5") {
+        $(`#editRatingOutput`).addClass("rating25");
+    } else if($(`#editMovieRating`).val() === "3") {
+        $(`#editRatingOutput`).addClass("rating3");
+    } else if($(`#editMovieRating`).val() === "3.5") {
+        $(`#editRatingOutput`).addClass("rating35");
+    } else if($(`#editMovieRating`).val() === "4") {
+        $(`#editRatingOutput`).addClass("rating4");
+    } else if($(`#editMovieRating`).val() === "4.5") {
+        $(`#editRatingOutput`).addClass("rating45");
+    } else if($(`#editMovieRating`).val() === "5") {
+        $(`#editRatingOutput`).addClass("rating5");
+    }
+})
+
+
 //function for ratings stars
 function ratingStars(rating){
     if(rating == 5){
@@ -496,3 +623,4 @@ function ratingStars(rating){
         return "../assets/0stars.png"
     }
 }
+
